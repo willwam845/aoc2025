5a8991933f8d06afc41e6a28c16dbf6edf393f07
wwm <willwam845@gmail.com>
2025-12-11 18:58:26 +0000

    feat: day 6 part 2, remove unnecessary enum in part 1

--- src/day06.rs
+++ src/day06.rs
@@ -1,34 +1,43 @@
-#[derive(Debug)]
-pub enum ProbElem {
-    Plus,
-    Times
-}
+use std::iter::repeat;
 
 pub fn part1(input: &str) -> u64 {
     let (value_lines, operation_line) = input.rsplit_once("\n").unwrap();
-    let operations: Vec<ProbElem> = operation_line.split_ascii_whitespace().map(|op| {
-        match op {
-            "+" => ProbElem::Plus,
-            "*" => ProbElem::Times,
-            _ => unreachable!()
-        }
-    }).collect();
+    let operations: Vec<&str> = operation_line.split_ascii_whitespace().collect();
     let values: Vec<u64> = value_lines.split_ascii_whitespace().map(|v| { v.parse::<u64>().unwrap() }).collect();
-    let ops: &[ProbElem] = &operations[..];
+    let ops = &operations[..];
     let vals = &values[..];
     let line_count = value_lines.lines().count();
     let problem_count = vals.len() / line_count;
     (0..problem_count).map(|i| {
         let problem_vals = (0..line_count).map(|l| { vals[i + problem_count * l] });
-        let op = &ops[i];
+        let op = ops[i];
         let f = match op {
-            ProbElem::Plus => |x, y| { x + y },
-            ProbElem::Times => |x, y| { x * y }
+            "+" => |x, y| { x + y },
+            "*" => |x, y| { x * y },
+            _ => unreachable!()
         };
         problem_vals.reduce(f).unwrap()
     }).sum()
 }
 
 pub fn part2(input: &str) -> u64 {
-    0
+    let (value_lines, operation_line) = input.rsplit_once("\n").unwrap();
+    let operations= operation_line.split_ascii_whitespace().into_iter();
+    let line_length = operation_line.len();
+    let mut values: Vec<_> = repeat(0u64).take(line_length).collect();
+    value_lines.lines().for_each(|line| {
+        values = line.chars().zip(values.iter()).map(|(i, v)| {
+            if i != ' ' { v * 10 + i.to_digit(10).unwrap() as u64 } 
+            else { *v }
+        }).collect()
+    });
+
+    values.split(|v| *v == 0).into_iter().zip(operations).map(|(vals, op)| {
+        let f = match op {
+            "+" => |x, y| { x + y },
+            "*" => |x, y| { x * y },
+            _ => unreachable!()
+        };
+        vals.iter().copied().reduce(f).unwrap()
+    }).sum()
 }
